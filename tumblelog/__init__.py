255a5b82801fbab2eb27c798a4f384a89888c965
another-elwood <worstaliasever@gmail.com>
2013-11-02 12:27:13 +0100

    added tumble log example from mongodb documentation

---
+++ tumblelog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+def register_blueprints(app):
+    # Prevents circular imports
+    from tumblelog.views import posts
+    app.register_blueprint(posts)
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+register_blueprints(app)
+
+if __name__ == '__main__':
+    app.run()
