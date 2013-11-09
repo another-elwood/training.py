f40ff85615a0ba7d68487ab7bc9fa005e20b4547
another-elwood <worstaliasever@gmail.com>
2013-11-09 11:55:27 +0100

    added new package 'traningbook'
    added package initialization
    added flask manager

---
+++ trainingbook/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'elwood'
+
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"DB": "trainingbook"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+
+db = MongoEngine(app)
+
+#def register_blueprints(app):
+#    # Prevents circular imports
+#    from tumblelog.views import posts
+#    from tumblelog.admin import admin
+#    app.register_blueprint(posts)
+#    app.register_blueprint(admin)
+#
+#register_blueprints(app)
+
+if __name__ == '__main__':
+    app.run()
