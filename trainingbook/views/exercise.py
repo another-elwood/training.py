fa97c5b9e1472579a9d3c7cb13c1156e9d6ea84e
another-elwood <worstaliasever@gmail.com>
2013-11-09 15:23:38 +0100

    added link targets to buttons
    added empty route targets 'add' and 'edit'

--- trainingbook/views/exercise.py
+++ trainingbook/views/exercise.py
@@ -1,6 +1,6 @@
 __author__ = 'elwood'
 
-from flask import Blueprint, request, redirect, render_template, url_for
+from flask import Blueprint, request, redirect, render_template, url_for, abort
 from flask.ext.mongoengine.wtf import model_form
 from trainingbook.models import Exercise
 
@@ -10,3 +10,12 @@ mod = Blueprint('exercises', __name__, url_prefix='/exercises', template_folder=
 def index():
      exercises = Exercise.objects.all()
      return render_template('exercises/list.html', exercises=exercises)
+
+@mod.route('/add')
+def add():
+    abort(404)
+
+@mod.route('/edit')
+def edit(id):
+    abort(404)
+
