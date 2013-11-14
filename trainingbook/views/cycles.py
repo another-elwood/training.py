20ca4f0895f8ce316ab886896d7cc2695018a1f3
another-elwood <worstaliasever@gmail.com>
2013-11-14 19:44:39 +0100

    added view skeleton for cycles
    added list template for cycles

---
+++ trainingbook/views/cycles.py
@@ -0,0 +1,23 @@
+__author__ = 'elwood'
+
+from flask import Blueprint, request, redirect, render_template, url_for, abort
+from flask.ext.mongoengine.wtf import model_form
+from trainingbook.models import Exercise, Cycle
+
+mod = Blueprint('cycles', __name__, url_prefix='/cycles', template_folder='templates')
+
+@mod.route('/')
+def index():
+    abort(404)
+
+@mod.route('/add', methods=['GET', 'POST'])
+def add():
+    abort(404)
+
+@mod.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    abort(404)
+
+@mod.route('/delete/<id>')
+def delete(id):
+    abort(404)
