16a7020e6759d58e64dffbaea80e4b6b36383aa2
another-elwood <worstaliasever@gmail.com>
2013-11-14 20:26:58 +0100

    implemented creation and editing of cycles

--- trainingbook/views/cycle.py
+++ trainingbook/views/cycle.py
@@ -13,12 +13,47 @@ def index():
 
 @mod.route('/add', methods=['GET', 'POST'])
 def add():
-    abort(404)
+    return get_page()
 
 @mod.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
-    abort(404)
+    return get_page(id)
 
 @mod.route('/delete/<id>')
 def delete(id):
     abort(404)
+
+def get_page(id=None):
+    context = get_context(id)
+
+    if request.method == 'POST':
+        form = context.get('form')
+        if form.validate():
+            cycle = context.get('cycle')
+            form.populate_obj(cycle)
+            cycle.save()
+            return redirect(url_for('cycles.index'))
+    return render_template('cycles/edit.html', **context)
+
+def get_context(id=None):
+    if id:
+        # edit
+        cycle = Cycle.objects.get_or_404(id=id)
+        form_cls = model_form(cycle.__class__)
+
+        if request.method == 'POST':
+            form = form_cls(request.form, inital=cycle._data)
+        else:
+            form = form_cls(obj=cycle)
+    else:
+        # create
+        cycle = Cycle()
+        form_cls = model_form(cycle.__class__)
+        form = form_cls(request.form)
+
+    context = {
+            "cycle": cycle,
+            "form": form,
+            "create": id is None
+        }
+    return context
