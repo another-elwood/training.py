81a4a715cfbe7889ec0a080704e9387169019f16
another-elwood <worstaliasever@gmail.com>
2013-11-21 19:39:40 +0100

    added sorting of planned exercises
    added handler to render new cycle template

--- trainingbook/views/cycle.py
+++ trainingbook/views/cycle.py
@@ -3,12 +3,15 @@ __author__ = 'elwood'
 from flask import Blueprint, request, redirect, render_template, url_for, abort
 from flask.ext.mongoengine.wtf import model_form
 from trainingbook.models import Exercise, Cycle
+import operator
 
 mod = Blueprint('cycles', __name__, url_prefix='/cycles', template_folder='templates')
 
 @mod.route('/')
 def index():
     cycles = Cycle.objects.all()
+    for cycle in cycles:
+        cycle.exercises.sort(key=operator.attrgetter('position'))
     return render_template('cycles/list.html', cycles=cycles)
 
 @mod.route('/add', methods=['GET', 'POST'])
@@ -17,13 +20,29 @@ def add():
 
 @mod.route('/edit/<id>', methods=['GET', 'POST'])
 def edit(id):
-    return get_page(id)
+    return load_cycle_page(id)
 
 @mod.route('/delete/<id>')
 def delete(id):
     cycle = Cycle.objects.get_or_404(id=id)
     cycle.delete()
     return redirect(url_for('cycles.index'))
+
+
+@mod.route('/test', methods=['GET'])
+def load_cycle_page(id):
+    exercises = Exercise.objects.all()
+
+    # sort exercises by position
+    cycle = Cycle.objects.get_or_404(id=id)
+    cycle.exercises.sort(key=operator.attrgetter('position'))
+
+    context = {
+            "cycle": cycle,
+            "exercises": exercises,
+        }
+
+    return render_template('cycles/new_edit.html', **context)
 
 def get_page(id=None):
     context = get_context(id)
