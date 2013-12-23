d1d151dd1d563bc7e279349d70b3aa87b12bc8a7
another-elwood <worstaliasever@gmail.com>
2013-12-23 13:49:10 +0100

    added list page for workouts
    added some method stubs to handle requests

--- trainingbook/views/workout.py
+++ trainingbook/views/workout.py
@@ -11,3 +11,15 @@ mod = Blueprint('workouts', __name__, url_prefix='/workouts', template_folder='t
 def index():
     workouts = Workout.objects.all()
     return render_template('workouts/list.html', workouts=workouts)
+
+@mod.route('/add', methods=['GET', 'POST'])
+def add():
+    abort(404)
+
+@mod.route('/view/<id>', methods=['GET'])
+def view(id):
+    abort(404)
+
+@mod.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id=None):
+    abort(404)
